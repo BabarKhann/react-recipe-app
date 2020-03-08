dde719876e6fd4ca3602314e68e49ad650d902b4
Babar Khan <babarkhan138@gmail.com>
2020-03-08 20:32:47 +0500

    Getting Recipies using Hooks.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const APP_ID = 'f135d347';
+  const APP_KEY = '278c6635f2609a823be0e5b8e87594e7';
+  // const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    setRecipes(data.hits);
+  }
+
   return (
-    <div className="App">
-      <h1>Hello React</h1>
+    <div className="App container mx-auto">
+      <form className="search-form">
+        <input className="bg-white focus:outline-none focus:shadow-outline border border-gray-300 rounded-lg py-2 px-4 block w-full appearance-none leading-normal"
+          type="text" placeholder="Search"
+        />
+        <button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          Search
+        </button>
+      </form>
     </div>
   );
 }
